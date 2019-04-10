4f3d5fa0033fccc014c327b6b41302c4c9d293a6
Williams, David <david.williams1@cgi.com>
2019-04-09 20:32:27 -0400

    End of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from "../_services/alertify.service";
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
     selector: 'app-nav',
@@ -10,26 +12,27 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
     model: any = {};
 
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
     ngOnInit() { }
 
     login() {
         this.authService.login(this.model).subscribe(next => {
-            console.log('Logged in successfully');
+            this.alertify.success('Logged in successfully');
         }, error => {
-            console.log(error);
-
+            this.alertify.error(error);
         });
     }
 
     loggedIn() {
-        const token = localStorage.getItem('token');
-        return !!token; // Return true if something in this token
+        return this.authService.loggedIn();
     }
 
     logout() {
         localStorage.removeItem('token');
-        console.log('Logout');
+        this.alertify.message('Logged out');
     }
+
+    
+
 }
